b2366a5a990a9cea9a490111f9b68af0d5a23728
Ali Kerem Yildirim <ali.kerem.yildirim1453@gmail.com>
2026-08-28 16:40:42 +0200

    Add files via upload
    Das erste Prototype von Choice-Game. Es handelt sich um das Tutorial und meine eigene Methode "myText"

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//
+
+
+Console.WriteLine("Geben Sie eine Name ein...");
+string Name = Console.ReadLine();
+
+Console.WriteLine("Hallo " + Name + " in diesem Spiel kannst du deine eigene" +
+    "Entscheidungen treffen (auch die Konsequenzen" );
